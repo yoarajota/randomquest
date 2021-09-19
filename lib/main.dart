67529cb5b5a3462a5105d591c929fb8ecf0f2717
João Vítor Sberse <jotabassosberse@gmail.com>
2021-09-19 16:26:11 -0300

    4x
    big one

--- lib/main.dart
+++ lib/main.dart
@@ -35,30 +35,30 @@ class _HomeState extends State<Home> {
   var pergunta = "randomquest";
   int _posicao1 = -1;
   int _posicao2 = -1;
-
   var lista1 = ['1', '2', '3', '4', '5']..shuffle();
   var lista2 = ['a', 'b', 'c', 'd', 'e']..shuffle();
 
-
-
 void MudaPosic1 (){
   _posicao1++;
+  if(_posicao1 == lista1.length) _posicao1 = 0; //start the loop over
   setState(() {
-  pergunta = lista1.elementAt(_posicao1);
-});
+      pergunta = lista1.elementAt(_posicao1);
+  });
+}
 
 void MudaPosic2 (){
   _posicao2++;
+  if(_posicao2 == lista2.length) _posicao2 = 0; //start the loop over
   setState(() {
-  pergunta = lista2.elementAt(_posicao2);
-});
-
+      pergunta = lista2.elementAt(_posicao2);
+  });
 }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(toolbarHeight: MediaQuery.of(context).size.width * 0.30, backgroundColor: Colors.black, toolbarOpacity: 0.5, title: Image.asset('images/logo.png', width: MediaQuery.of(context).size.width * 0.20, fit: BoxFit.cover,)),
+        appBar: AppBar(toolbarHeight: MediaQuery.of(context).size.height * 0.20, backgroundColor: Colors.black, toolbarOpacity: 0.5, title: Image.asset('images/logo.png', width: MediaQuery.of(context).size.width * 0.20, fit: BoxFit.cover,)),
         body: Center(
           child: Column(
             children: [
@@ -79,17 +79,15 @@ void MudaPosic2 (){
                     highlightColor: Colors.transparent,
                     icon: Image.asset('images/eye.png'),
                     iconSize: 150,
-                    onPressed: () {
-                      _posicao2++;
-                    setState(() {MudaPosic2();});
+                    onPressed: () {MudaPosic2();
                       }, 
                     color: Colors.black, 
-                      )
+                  ),
                 ],
               ),
-            SizedBox(height: MediaQuery.of(context).size.width * 0.60),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.30),
             SizedBox(height: 1, child: Container(color: Colors.black,),),
-            SizedBox(height: MediaQuery.of(context).size.width * 0.17),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.10),
             Text(pergunta, style: const TextStyle(fontSize: 20)
             )], 
           ),
@@ -102,4 +100,3 @@ void MudaPosic2 (){
     // TODO: implement build
     throw UnimplementedError();
   }
-}
